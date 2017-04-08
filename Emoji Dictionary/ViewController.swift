b251cdf77db1755bbbe3eeeb48fd26a9057b78a2
Brandon Viertel <bviertel@gmail.com>
2017-04-08 17:13:20 -0400

    CHANGE: Table DataSource and Delegate stuff

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -2,17 +2,26 @@
 //  ViewController.swift
 //  Emoji Dictionary
 //
-//  Created by Ann Marie Seyerlein on 4/8/17.
+//  Created by Brandon Viertel on 4/8/17.
 //  Copyright © 2017 Brandon. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+/* Needed to add UITableViewDataSource and UITableViewDelegate due to use below. Note how they were added in. */
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // Connection to table code from StoryBoard
+    @IBOutlet weak var dacooltableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Where is the information coming from? (See note above ViewController)
+        dacooltableview.dataSource = self
+        dacooltableview.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
