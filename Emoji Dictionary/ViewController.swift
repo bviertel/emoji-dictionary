644435023b4fbdb11405c60d534776a0a9088476
Brandon Viertel <bviertel@gmail.com>
2017-04-08 18:04:06 -0400

    CHANGE: Added navigation controller, and moveSegue thing

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -47,6 +47,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
         
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "moveSegue", sender: "🛶")
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
