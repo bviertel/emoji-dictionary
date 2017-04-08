ff8e55ad47529b473b94e7c43cd9c3e00c31c642
Brandon Viertel <bviertel@gmail.com>
2017-04-08 17:46:50 -0400

    CHANGE: Added second view controller

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // Connection to table code from StoryBoard
     @IBOutlet weak var dacooltableview: UITableView!
     
+    // Creating an Array to store everything in the table
+    var emojis = ["😀","😋","😎","👽","😈","✌🏼","👨‍👩‍👦‍👦","🐱"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,17 +30,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // How many rows in Table View? The '-> Int' is the Return function!
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 10
+        return emojis.count
         
     }
     
     // What do you want inside of each row of Table View? Also, ROW = CELL! Note the Return!
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        print(indexPath.row)
+        
         let cell = UITableViewCell()
         
         // Every cell will have 'HELLO THERE' as label. Note the question mark
-        cell.textLabel?.text = "HELLO THERE"
+        cell.textLabel?.text = emojis[indexPath.row]
         
         return cell
         
